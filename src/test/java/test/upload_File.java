16e0b355559fac226d984aa157cade5885a481f1
Winner_Akshay <Winner_Akshay@DESKTOP-VCK713F>
2023-04-08 15:04:07 +0530

    test

---
+++ src/test/java/test/upload_File.java
@@ -0,0 +1,64 @@
+package test;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class upload_File {
+
+	@Test
+	public void one() throws InterruptedException, IOException
+	{
+ChromeOptions co = new ChromeOptions();
+co.addArguments("--remote-allow-origins=*");
+co.addArguments("-start-maximized");
+		WebDriver driver=new ChromeDriver(co);
+		
+		driver.manage().deleteAllCookies();
+
+		//driver.navigate().to("https://demos.devexpress.com/aspxeditorsdemos/Features/NullText.aspx");
+		driver.get("https://demo.automationtesting.in/Register.html");
+		Thread.sleep(4000);
+		WebElement up = driver.findElement(By.id("imagesrc"));
+		Actions act = new Actions(driver);
+		act.doubleClick(up).perform();
+		Thread.sleep(2000);
+		Runtime.getRuntime().exec("C:\\Users\\User\\Desktop\\abc.exe");//to exe 
+/*		ControlFocus("Open","","Edit1");   for scite script
+		ControlSetText("Open","","Edit1","C:\Users\User\Desktop\NTS FORM.png");
+		ControlClick("Open","","Button1");
+*/
+
+		Thread.sleep(2000);
+		
+		Runtime.getRuntime().exec("C:\\Users\\User\\Desktop\\abc.exe");
+		driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Yogesh");
+		driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("Dangade");
+		driver.findElement(By.xpath("//textarea[@class='form-control ng-pristine ng-untouched ng-valid']")).sendKeys("Shrigonda");
+		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("abc@gmail.com");
+		driver.findElement(By.xpath("//input[@type='tel']")).sendKeys("8888024539");
+		driver.findElement(By.name("radiooptions")).click();
+		driver.findElement(By.id("submitbtn")).click();
+		WebElement c = driver.findElement(By.xpath("//select[@id='countries']"));
+		Select se = new Select(c);
+		List<WebElement> op = se.getOptions();
+		for (int i=0;i<op.size();i++)
+		{
+			String txt = op.get(i).getText();
+			System.out.println(txt);
+		}
+		
+			
+		
+		//driver.quit();
+	}
+
+}
---
+++ src/test/java/test/windowHandle.java
@@ -0,0 +1,94 @@
+package test;
+
+import java.time.Duration;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class windowHandle {
+	@Test
+	public void wondow()
+	{
+		ChromeOptions co = new ChromeOptions();
+		
+		co.addArguments("--remote-allow-origins=*");
+		co.addArguments("-start-maximized");
+		co.setImplicitWaitTimeout(Duration.ofSeconds(30));
+		ChromeDriver driver = new ChromeDriver(co);
+		driver.get("https://demo.automationtesting.in/Windows.html");
+		driver.findElement(By.partialLinkText("click")).click();
+		String parentWindow = driver.getWindowHandle();
+		Set<String> newWindow = driver.getWindowHandles();
+		for(String WindowHandle:newWindow)
+		{
+			if(!WindowHandle.equals(parentWindow))
+			{
+				driver.switchTo().window(WindowHandle);
+			}
+			else
+			{
+				
+			}
+		}
+		String pgTitle = driver.getTitle();
+		if(pgTitle.equals("Selenium"))
+		{
+			System.out.println("Page title of new window is verify "+"Selenium");
+		}
+		else
+		{
+			System.out.println("control is not switch");
+		}
+		
+		driver.findElement(By.xpath("//a[contains(@class,'selenium-button selenium-we')]")).click();
+		
+		String pgTitle1 = driver.getTitle();
+		if(pgTitle1.equals("WebDriver | Selenium"))
+		{
+			System.out.println("Page title of WebDriver is verify "+"WebDriver | Selenium");
+		}
+		else
+		{
+			System.out.println("not click");
+		}
+	/*	WebElement api = driver.findElement(By.partialLinkText("Actions API"));
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		org.openqa.selenium.Point point = api.getLocation();
+		int xAxix = point.getX();
+		int yAxix = point.getY();
+		
+		jse.executeScript("window.scrollBy("+xAxix+","+yAxix+")");
+		*/
+		driver.findElement(By.xpath("//a[.='Watch the Videos']")).click();
+		String secondWindow = driver.getWindowHandle();
+		
+		Set<String> allHDl = driver.getWindowHandles();
+		
+		for(String newWindow2:allHDl)
+		{
+			if (!newWindow2.equals(secondWindow))
+			{
+				if(!newWindow2.equals(parentWindow))
+				{
+				driver.switchTo().window(newWindow2);
+				String ss = driver.getTitle();
+				System.out.println(ss);
+				driver.findElement(By.xpath("(//div[@class='yt-spec-touch-feedback-shape__fill'])[4]")).click();
+				
+			}
+			}
+			else
+			{
+				
+			}
+		}
+		
+		
+	}
+
+}
